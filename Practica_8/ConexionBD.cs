48eb6054a657103c58aa1e5a7ec6c1cd465524f3
Jesus Eduardo Ramirez Palos <jesus.ramirez18@tectijuana.edu.mx>
2020-05-06 21:08:19 -0700

    Agregado

---
+++ Practica_8/ConexionBD.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//libreria para usar sql 
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.Sql;
+
+
+namespace Practica_8
+{
+    class ConexionBD
+    {
+        //Creacion de conexion
+        SqlConnection ConertarBD;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        SqlDataAdapter da;
+        DataTable dt;
+
+        //Conectamos y abrirmos la base de datos
+        public ConexionBD()
+        {
+            try
+            {
+                //se conectar ala base de datos
+                ConertarBD=new SqlConnection("Data Source=DESKTOP-AQ0I65S\\SQLEXPRESS;Initial Catalog=PracticaTAP;Integrated Security=True");
+                //se abre la base de datos
+                ConertarBD.Open();
+                //confirmacion de que fue abierta
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se conecto con la base de datos: " + ex.ToString());
+            }
+        }
+        //Incertar datos en la base de datos
+        public string Insertar(int Id, string Nombre, string Apellido, string FechaNacimiento)
+        {
+            string salida = "Valor ingresado";
+            try
+            {
+                ConertarBD = new SqlConnection("Data Source=DESKTOP-AQ0I65S\\SQLEXPRESS;Initial Catalog=PracticaTAP;Integrated Security=True");
+                ConertarBD.Open();
+                cmd = new SqlCommand("insert into [Personas] values(" + Id + ",'" + Nombre + "','" + Apellido + "','" + FechaNacimiento + "')",ConertarBD);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show(" Datos ingresados ");
+            }
+            catch (Exception ex)
+            {
+
+                salida = "No se Pudo insertar el valor " + ex.ToString();
+            }
+            ConertarBD.Close();
+            return salida;      
+        }
+
+        
+        //validacion para ver que no se repitan los datos
+        public int Registros(int Id)
+        {
+            //tipo de dato que retornamoss
+            int contador = 0;
+            try
+            {
+                //seleciona todos los datos de la tabla que este el id
+                cmd = new SqlCommand("Select * from Personas where Id=" + Id + "", ConertarBD);
+                //nos regresa el numero de personas que tenga un identificador 
+                dr = cmd.ExecuteReader();
+                //recorre el numero de veces que obtierne el registrO
+                while (dr.Read())
+                {
+                    contador++;
+                }
+                dr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se Encontro Consulta" + ex.ToString());
+            }
+            return contador;
+        }
+        public void cargarPersonas(DataGridView DgvPersonas)
+        {
+            try
+            {
+                da = new SqlDataAdapter("Select * from Personas", ConertarBD);
+                dt = new DataTable();
+                da.Fill(dt);
+                DgvPersonas.DataSource = dt;
+                //Para auto ajustar los cuadros o celdas
+                //DgvPersonas.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo llenar el Datagridview: " + ex.ToString());
+            }
+        }
+
+
+        public void llenarTexBoxConsulta(int id, TextBox txtNombre, TextBox txtApellidos, DateTimePicker dtpFecha)
+        {
+            try
+            {
+                cmd = new SqlCommand("Select * from Personas where Id=" + id + "", ConertarBD);
+                dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    txtNombre.Text = dr["Nombre"].ToString();
+                    txtApellidos.Text = dr["Apellidos"].ToString();
+                    dtpFecha.Text = dr["FechaNacimiento"].ToString();
+
+                }
+                dr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo llenar los campos: " + ex.ToString());
+            }
+        }
+
+        public string atualizar(int Id, string Nombre, string Apellido, string FechaNacimiento)
+        {
+            string salida = "Se actualizaron los datos";
+            try
+            {
+                cmd = new SqlCommand("Update Personas set Nombre ='" + Nombre + "' ,Apellidos='" + Apellido + "', FechaNacimiento='" + FechaNacimiento + "' where Id=" + Id + "", ConertarBD);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show(" Datos Actualizados ");
+            }
+            catch (Exception ex)
+            {
+                salida = "No se actualizo: " + ex.ToString();
+            }
+            return salida;
+        }
+        public string Eliminar(int Id)
+        {
+            string eliminar = "Se Eliminaron los Datos Correctamente";
+            try
+            {
+                cmd = new SqlCommand("delte from Personas where Id =" + Id + "", ConertarBD);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show(" Datos Actualizados ");
+            }
+            catch (Exception ex)
+            {
+                eliminar = " Erro al Eliminar datos " + ex.ToString();
+            }
+            return eliminar;
+        }
+
+
+    }
+}
---
+++ Practica_8/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Practica_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RbInsertar = new System.Windows.Forms.RadioButton();
+            this.RbEliminar = new System.Windows.Forms.RadioButton();
+            this.RbModificar = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.DtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.CmdModificar = new System.Windows.Forms.Button();
+            this.CmdEliminar = new System.Windows.Forms.Button();
+            this.CmdAgregar = new System.Windows.Forms.Button();
+            this.TxtApellidos = new System.Windows.Forms.TextBox();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.TxtId = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DgvPersonas = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvPersonas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RbInsertar
+            // 
+            this.RbInsertar.AutoSize = true;
+            this.RbInsertar.Location = new System.Drawing.Point(6, 35);
+            this.RbInsertar.Name = "RbInsertar";
+            this.RbInsertar.Size = new System.Drawing.Size(67, 17);
+            this.RbInsertar.TabIndex = 1;
+            this.RbInsertar.TabStop = true;
+            this.RbInsertar.Text = "Registrar";
+            this.RbInsertar.UseVisualStyleBackColor = true;
+            this.RbInsertar.CheckedChanged += new System.EventHandler(this.RbInsertar_CheckedChanged);
+            // 
+            // RbEliminar
+            // 
+            this.RbEliminar.AutoSize = true;
+            this.RbEliminar.Location = new System.Drawing.Point(79, 35);
+            this.RbEliminar.Name = "RbEliminar";
+            this.RbEliminar.Size = new System.Drawing.Size(61, 17);
+            this.RbEliminar.TabIndex = 2;
+            this.RbEliminar.TabStop = true;
+            this.RbEliminar.Text = "Eliminar";
+            this.RbEliminar.UseVisualStyleBackColor = true;
+            this.RbEliminar.CheckedChanged += new System.EventHandler(this.RbEliminar_CheckedChanged);
+            // 
+            // RbModificar
+            // 
+            this.RbModificar.AutoSize = true;
+            this.RbModificar.Location = new System.Drawing.Point(146, 35);
+            this.RbModificar.Name = "RbModificar";
+            this.RbModificar.Size = new System.Drawing.Size(68, 17);
+            this.RbModificar.TabIndex = 3;
+            this.RbModificar.TabStop = true;
+            this.RbModificar.Text = "Modificar";
+            this.RbModificar.UseVisualStyleBackColor = true;
+            this.RbModificar.CheckedChanged += new System.EventHandler(this.RbModificar_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.RbInsertar);
+            this.groupBox1.Controls.Add(this.RbModificar);
+            this.groupBox1.Controls.Add(this.RbEliminar);
+            this.groupBox1.Location = new System.Drawing.Point(105, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(221, 83);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Opciones";
+            // 
+            // DtpFecha
+            // 
+            this.DtpFecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.DtpFecha.Location = new System.Drawing.Point(121, 213);
+            this.DtpFecha.Name = "DtpFecha";
+            this.DtpFecha.Size = new System.Drawing.Size(195, 20);
+            this.DtpFecha.TabIndex = 25;
+            // 
+            // CmdModificar
+            // 
+            this.CmdModificar.Location = new System.Drawing.Point(340, 190);
+            this.CmdModificar.Name = "CmdModificar";
+            this.CmdModificar.Size = new System.Drawing.Size(75, 23);
+            this.CmdModificar.TabIndex = 24;
+            this.CmdModificar.Text = "Modificar";
+            this.CmdModificar.UseVisualStyleBackColor = true;
+            this.CmdModificar.Click += new System.EventHandler(this.CmdModificar_Click);
+            // 
+            // CmdEliminar
+            // 
+            this.CmdEliminar.Location = new System.Drawing.Point(340, 161);
+            this.CmdEliminar.Name = "CmdEliminar";
+            this.CmdEliminar.Size = new System.Drawing.Size(75, 23);
+            this.CmdEliminar.TabIndex = 23;
+            this.CmdEliminar.Text = "Eliminar";
+            this.CmdEliminar.UseVisualStyleBackColor = true;
+            this.CmdEliminar.Click += new System.EventHandler(this.CmdEliminar_Click);
+            // 
+            // CmdAgregar
+            // 
+            this.CmdAgregar.Location = new System.Drawing.Point(340, 129);
+            this.CmdAgregar.Name = "CmdAgregar";
+            this.CmdAgregar.Size = new System.Drawing.Size(75, 23);
+            this.CmdAgregar.TabIndex = 22;
+            this.CmdAgregar.Text = "Agregar";
+            this.CmdAgregar.UseVisualStyleBackColor = true;
+            this.CmdAgregar.Click += new System.EventHandler(this.CmdAgregar_Click);
+            // 
+            // TxtApellidos
+            // 
+            this.TxtApellidos.Location = new System.Drawing.Point(121, 187);
+            this.TxtApellidos.Name = "TxtApellidos";
+            this.TxtApellidos.Size = new System.Drawing.Size(195, 20);
+            this.TxtApellidos.TabIndex = 21;
+            // 
+            // TxtNombre
+            // 
+            this.TxtNombre.Location = new System.Drawing.Point(121, 158);
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(195, 20);
+            this.TxtNombre.TabIndex = 20;
+            // 
+            // TxtId
+            // 
+            this.TxtId.Location = new System.Drawing.Point(121, 126);
+            this.TxtId.Name = "TxtId";
+            this.TxtId.Size = new System.Drawing.Size(195, 20);
+            this.TxtId.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Fecha Nacimiento:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Apellidos:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Nombre:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Id:";
+            // 
+            // DgvPersonas
+            // 
+            this.DgvPersonas.BackgroundColor = System.Drawing.Color.White;
+            this.DgvPersonas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvPersonas.Location = new System.Drawing.Point(22, 305);
+            this.DgvPersonas.Name = "DgvPersonas";
+            this.DgvPersonas.Size = new System.Drawing.Size(393, 150);
+            this.DgvPersonas.TabIndex = 26;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 611);
+            this.Controls.Add(this.DgvPersonas);
+            this.Controls.Add(this.DtpFecha);
+            this.Controls.Add(this.CmdModificar);
+            this.Controls.Add(this.CmdEliminar);
+            this.Controls.Add(this.CmdAgregar);
+            this.Controls.Add(this.TxtApellidos);
+            this.Controls.Add(this.TxtNombre);
+            this.Controls.Add(this.TxtId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvPersonas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton RbInsertar;
+        private System.Windows.Forms.RadioButton RbEliminar;
+        private System.Windows.Forms.RadioButton RbModificar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker DtpFecha;
+        private System.Windows.Forms.Button CmdModificar;
+        private System.Windows.Forms.Button CmdEliminar;
+        private System.Windows.Forms.Button CmdAgregar;
+        private System.Windows.Forms.TextBox TxtApellidos;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.TextBox TxtId;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView DgvPersonas;
+    }
+}
+
